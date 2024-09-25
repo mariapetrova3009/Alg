8398d0dd366b52d244661e0c3d2f08fc37eacef4
mariapetrova3009 <petrovamaria3009@gmail.com>
2024-09-25 12:43:46 +0300

    Добавлено решение для задачи 1

---
+++ lab1/1/1.py
@@ -0,0 +1,15 @@
+with open('input.txt') as f:
+    n = int(f.readline())
+    array = list(map(int, f.readline().split()))
+    for i in range(1, n):
+        x = array[i]
+        j = i
+
+        while j > 0 and array[j - 1] > x:
+            array[j] = array[j - 1]
+            j -= 1
+
+        array[j] = x
+
+with open('output.txt', 'w') as f:
+    f.write(' '.join(map(str, array)))
